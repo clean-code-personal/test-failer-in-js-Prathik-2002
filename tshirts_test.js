d407c72d98e974ba49d30a4b02a394fcfdfdebbf
Prathik-2002 <acharyaprathik4@gmail.com>
2024-02-07 12:41:41 +0530

    tshirts.js split into 2 files: tshirts.js and tshirts_test.js

---
+++ tshirts_test.js
@@ -0,0 +1,9 @@
+const {expect} = require('chai');
+const {size} = require('./tshirts.js')
+
+expect(size(37)).equals('S'); 
+expect(size(40)).equals('M');
+expect(size(41)).equals('M');
+expect(size(42)).equals('L');
+expect(size(43)).equals('L');
+console.log('All is well (maybe!)');
