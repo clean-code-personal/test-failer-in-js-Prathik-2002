ab1ec49da95b790b122f8576975c70e966a870f4
Prathik-2002 <acharyaprathik4@gmail.com>
2024-02-07 12:42:30 +0530

    testcases added

--- tshirts_test.js
+++ tshirts_test.js
@@ -2,6 +2,7 @@ const {expect} = require('chai');
 const {size} = require('./tshirts.js')
 
 expect(size(37)).equals('S'); 
+expect(size(38)).equals('S'); //38 is a size "S"
 expect(size(40)).equals('M');
 expect(size(41)).equals('M');
 expect(size(42)).equals('L');
