80a18ab23d42ad4fced94aa52ee7c34204d89e2a
Prathik-2002 <acharyaprathik4@gmail.com>
2024-02-08 11:19:04 +0530

    testPairNumberColorpair function added

--- misaligned_test.js
+++ misaligned_test.js
@@ -1,16 +1,31 @@
 const {expect} = require('chai')
 const {getcolor_map, print_color_map} = require("./misaligned");;
+function testPairNumberColorPair(pairNumber, majorColor, minorColor){
+    if(pairNumber<1 || pairNumber>25){
+        return false
+    }
+    const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
+    const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+    if(majorColor == majorColors[(pairNumber - 1)/ 5] && minorColor == minorColors[(pairNumber - 1)%5]){
+        return true
+    }
+    return false
+}
+function testUniqueColorPair(colorMap){
+    colorMap.forEach(colorPair => {
+        if(colorMap.indexOf(colorPair) != colorMap.toReversed().indexOf(colorPair)){
+            return false
+        }
+    })
+    return true
+}
+
+
 result = print_color_map();
 expect(result).equals(25);
+getcolor_map().forEach(element=>{
+    expect(testPairNumberColorPair(element.pairNumber,element.majorColor,element.minorColor)).equals(true)
+})
+expect(testUniqueColorPair(getcolor_map())).equals(true);
 
 console.log('All is well (maybe!)');
-
-function split_color_map_into_object(color_map){
-    const split_color = color_map.split("\n")
-    console.log(split_color)
-}
-split_color_map_into_object(result)
-
-function testPairNumberColorPair(pairNumber, majorColor, minorColor){
-    
-}
