74383a093616f2a70a80707f63d3195371db7e49
Prathik-2002 <acharyaprathik4@gmail.com>
2024-02-09 06:33:33 +0530

    test to check alignment added

--- misaligned.js
+++ misaligned.js
@@ -1,4 +1,8 @@
 let color_map = []
+let color_map_string = []
+function getcolor_map_string(){
+    return color_map_string
+}
 function getcolor_map(){
     return color_map
 }
@@ -7,11 +11,13 @@ function print_color_map() {
     const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
     for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
-            color_map.push({"paiNumber":(i * 5 + j),"majorColor":majorColors[i],"minorColor":minorColors[j]})
-            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`)
+            const colorPairnumberColorPair = {"paiNumber":(i * 5 + j),"majorColor":majorColors[i],"minorColor":minorColors[j]}
+            color_map_string.push(`${colorPairnumberColorPair.paiNumber} | ${colorPairnumberColorPair.majorColor} | ${colorPairnumberColorPair.minorColor}`)
+            color_map.push(colorPairnumberColorPair)
         }
     }
+    console.log(color_map_string.join("\n"))
     return majorColors.length * minorColors.length;
 }
-module.exports = {getcolor_map, print_color_map}
+module.exports = {getcolor_map, getcolor_map_string, print_color_map}
 
--- misaligned_test.js
+++ misaligned_test.js
@@ -1,5 +1,5 @@
 const {expect} = require('chai')
-const {getcolor_map, print_color_map} = require("./misaligned");;
+const {getcolor_map, getcolor_map_string, print_color_map} = require("./misaligned");;
 function testPairNumberColorPair(pairNumber, majorColor, minorColor){
     if(pairNumber<1 || pairNumber>25){
         return false
@@ -13,22 +13,33 @@ function testPairNumberColorPair(pairNumber, majorColor, minorColor){
 }
 function testUniqueColorPair(colorMap){
     for( const color in colorMap ){
-        console.log(colorMap.indexOf(color) != colorMap.lastIndexOf(color))
         if(colorMap.indexOf(color) != colorMap.lastIndexOf(color)){
             return false
         }
     }
     return true
 }
+function getSeparatorPosition(colorString){
+    return [colorString.indexOf("|"), colorString.lastIndexOf("|")]
+}
+function testAllAlignmentsBasedOnFirstRow(colorMapString){
+    const positionOfIrstSeparatorInFirstRow = colorMapString[0].indexOf("|")
+    const positionOfSecondSeparatorInFirstRow = colorMapString[0].lastIndexOf("|")
+    colorMapString.forEach(color =>{
+        const separatorPosition = getSeparatorPosition(color)
+        expect(positionOfIrstSeparatorInFirstRow).equals(separatorPosition[0])
+        expect(positionOfSecondSeparatorInFirstRow).equals(separatorPosition[1])
+    
+    })
+}
+
+
 
 result = print_color_map();
-console.log(getcolor_map())
 expect(result).equals(25);
-console.log("25")
+testAllAlignmentsBasedOnFirstRow(getcolor_map_string())
 expect(testUniqueColorPair(getcolor_map())).equals(true);
-console.log("uniue")
 getcolor_map().forEach(element=>{
     expect(testPairNumberColorPair(element.pairNumber,element.majorColor,element.minorColor)).equals(true)
 })
-
 console.log('All is well (maybe!)');
