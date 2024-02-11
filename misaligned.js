a89013b3aa6401563e799e15cbf0dd59d322fd4d
Prathik-2002 <acharyaprathik4@gmail.com>
2024-02-11 23:35:12 +0530

    fixes

--- misaligned.js
+++ misaligned.js
@@ -6,18 +6,27 @@ function getcolor_map_string(){
 function getcolor_map(){
     return color_map
 }
+function getLengthOfLongestString(list){
+    const longestString = list.reduce((a,b) => {
+        return a.length > b.length ? a : b;
+    })
+    return longestString.length
+}
 function print_color_map() {
     const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
     const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
-    let minor_majorColorSpacing =  majorColors.reduce((a, b) => {
-            return a.length > b.length ? a : b; //finds longest string in the list
-        }).length + 1;
-    let pairNumberSpacing = String(minorColors.length * majorColors.length).length + 1
+    const pairNumberSpacing = String(minorColors.length * majorColors.length).length + 1
+    const majorColorSpacing = getLengthOfLongestString(majorColors) + 1
+    const minorColorSpacing = getLengthOfLongestString(minorColors) + 1
+
     for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
-            const PairnumberColorPair = {"pairNumber":(i * 5 + j + 1),"majorColor":majorColors[i],"minorColor":minorColors[j]}
-            color_map_string.push(String(PairnumberColorPair.pairNumber).padEnd(pairNumberSpacing) + "| " + (PairnumberColorPair.majorColor).padEnd(minor_majorColorSpacing) + "| " + (PairnumberColorPair.minorColor).padEnd( minor_majorColorSpacing))
-            color_map.push(PairnumberColorPair)
+            const PairnumberColorPairMap = {"pairNumber":(i * 5 + j + 1),"majorColor":majorColors[i],"minorColor":minorColors[j]}
+            const pairNumberAlignedString = String(PairnumberColorPairMap.pairNumber).padEnd(pairNumberSpacing)
+            const majorColorAlignedString = (PairnumberColorPairMap.majorColor).padEnd(majorColorSpacing)
+            const minorColorAlignedString = (PairnumberColorPairMap.minorColor).padEnd(minorColorSpacing)
+            color_map_string.push(`${pairNumberAlignedString}| ${majorColorAlignedString}| ${minorColorAlignedString}`)
+            color_map.push(PairnumberColorPairMap)
         }
     }
     console.log(color_map_string.join("\n"))
